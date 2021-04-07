6d57b54b1d3447c3c0bdd1919aed62bc04bb8f40
José Antoio <afana500@gmail.com>
2021-04-07 17:44:09 +0200

    Optimizado el codigo de los enemigos
    Ahora los enemigos azules y rojos comparten el mismo script

---
+++ Assets/Scripts/Jumbo.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jumbo : MonoBehaviour
+{
+    public string color = "";
+    public float velocidad = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Translate(0, -velocidad * Time.deltaTime, 0);
+    }
+
+
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+
+        if (collision.gameObject.tag == "ProyectilRojo" && color.Equals("rojo"))
+        {
+            gameObject.GetComponent<Animator>().SetTrigger("Tocado");
+
+            Destroy(gameObject, 0.2f);
+        }
+        if (collision.gameObject.tag == "ProyectilAzul" && color.Equals("azul"))
+        {
+            gameObject.GetComponent<Animator>().SetTrigger("Tocado");
+
+            Destroy(gameObject, 0.2f);
+        }
+    }
+}
