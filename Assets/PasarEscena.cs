b2cdac0fe298faae0cefffc82720103c3069924e
José Antoio <afana500@gmail.com>
2021-05-26 13:34:46 +0200

    Creada transición entre escenas

---
+++ Assets/PasarEscena.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PasarEscena : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("Esperar");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+
+    public IEnumerator Esperar()
+    {
+        yield return new WaitForSeconds(9.5f);
+        SceneManager.LoadScene("SampleScene");
+    }
+}
